67c51176073c0e449bd17fe00418d2271650dff7
Yagnesh Modh <contact@thespecialcharacter.com>
2023-11-08 14:47:30 +0530

    commit code

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,43 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
+const devMode = process.env.NODE_ENV !== "production";
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, "output"),
-        filename: "bundle.js"
-    },
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use:["style-loader", "css-loader", "postcss-loader"]
-            }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: 'index.html',
-    })],
-    devServer: {
-        open: true,
-        port: 3000
-    }
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "output"),
+    filename: "bundle.js",
+  },
+  mode: devMode ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          ,
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+  devServer: {
+    open: true,
+    port: 3000,
+  },
+};
