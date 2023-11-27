71261dd6f4ee40227e7a65fbbc57bc543d18efe2
Yagnesh Modh <contact@thespecialcharacter.com>
2023-11-27 11:07:00 +0530

    clearn branch

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const sum = (a, b) => a + b;
-
-const sub = (a, b) => a - b;
-
-module.exports = {
-  sum,
-  sub,
-};
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.js", "./public/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
