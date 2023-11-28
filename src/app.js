019c5db2dfdef18b4335e9ee2f4fadc3f878c6cd
Yagnesh Modh <contact@thespecialcharacter.com>
2023-11-28 11:40:59 +0530

    commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+module.exports = {
+    add, 
+    sub
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { add, sub } = require("./app");
+require("./style.css");
+
+console.log("hello world");
+
+const arr = [1,2,3,4]
+
+const [a, b] = arr;
+
+console.log(a);
+console.log(b);
+
+// commented code
+
+console.log(add(1, 2));
+console.log(sub(2, 1));
