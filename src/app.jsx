cfea951f6598a54464fa075fd300827ade74cf46
Yagnesh Modh <contact@thespecialcharacter.com>
2023-12-08 11:19:00 +0530

    commit code

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Child1 from './child1';
 import Child2 from './child2';
-import Child3 from './child3';
 
-function App({ title, desc, age, gender }) {
-  console.log('app render');
-  return (
-    <>
-      <h1 className="bg-yellow-400 text-blue-600">{`hello ${title}`}</h1>
-      <h2>{desc}</h2>
-      {age && <h3>{age}</h3>}
-      {gender && <h4>{gender}</h4>}
-      <input type="text" />
-      <input type="checkbox" />
-      <button type="button">Submit</button>
-      <Child1 />
-      <Child2 />
-      <Child3 />
-    </>
-  );
+// Child2.getDerivedStateFromProps = (props, state) => ({
+//   greet: `hello ${props.name}`,
+// });
+
+class App extends Component {
+  state = {
+    name: 'yagnesh',
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <>
+        <Child2 name={name} />
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ name: 'rohit' });
+          }}
+        >
+          Change name
+        </button>
+
+        <p>app component name = {name}</p>
+      </>
+    );
+  }
 }
+
+// function App({ title, desc, age, gender }) {
+//   console.log('app render');
+//   return (
+//     <>
+//       <h1 className="bg-yellow-400 text-blue-600">{`hello ${title}`}</h1>
+//       <h2>{desc}</h2>
+//       {age && <h3>{age}</h3>}
+//       {gender && <h4>{gender}</h4>}
+//       <input type="text" />
+//       <input type="checkbox" />
+//       <button type="button">Submit</button>
+//       <Child1 />
+//       <Child2 name="yagnesh" />
+//       <Child3 />
+//     </>
+//   );
+// }
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
--- src/child2.jsx
+++ src/child2.jsx
@@ -1,38 +1,89 @@
 import React, { Component } from 'react';
 
-export default class Child2 extends Component {
-  state = {
-    count: 0,
-  };
+// Mounting
 
+// 1. constructor
+
+// Updating
+
+// unmounting
+
+// error
+
+export default class Child2 extends Component {
+  // base on props define state value
+  // method binding
   constructor(props) {
     super(props);
+
+    console.log(props);
+
+    console.log('constructor child 2');
     this.state = {
       count: 0,
+      productInfo: null,
+      // greet: `hello ${props.name}`,
+    };
+
+    // analytics
+  }
+
+  // derive state value base on props
+  // this life cycle mehtod call everytime whenever props or state value change;
+  static getDerivedStateFromProps(props, state) {
+    return {
+      greet: 'hello',
     };
   }
 
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://fakestoreapi.com/products/1');
+      const json = await res.json();
+
+      console.log(json);
+      this.setState({ productInfo: json });
+
+      document.getElementById('');
+
+      document.getElementById('heading1').addEventListener('copy', () => {
+        console.log('text copied');
+      });
+    } catch (error) {}
+  }
+
+  changeGreet = () => {
+    this.setState((state, props) => ({
+      greet: `Bonjour ${props.name}`,
+    }));
+  };
+
+  increment = () => {
+    this.setState(({ count }) => ({ count: count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(({ count }) => ({ count: count - 1 }));
+  };
+
   render() {
-    const { count } = this.state;
+    console.log('render child 2');
+    const { count, greet, productInfo } = this.state;
+
+    console.log(productInfo);
 
     return (
       <div>
-        <button
-          type="button"
-          onClick={() => {
-            // this.setState({ count: 5 });
-            this.setState(({ count }) => ({ count: count + 1 }));
-          }}
-        >
+        {productInfo?.title && <h1 id="heading">{productInfo.title}</h1>}
+        <h2 id="heading1">hello world</h2>
+        <button type="button" onClick={this.changeGreet}>
+          Change Greet
+        </button>
+        <button type="button" onClick={this.increment}>
           +
         </button>
         <p>{count}</p>
-        <button
-          type="button"
-          onClick={() => {
-            this.setState(({ count }) => ({ count: count - 1 }));
-          }}
-        >
+        <button type="button" onClick={this.decrement}>
           -
         </button>
       </div>
