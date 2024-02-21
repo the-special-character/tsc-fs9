68e0ec94907b1a4e5278444759d355abd21ae5b3
Yagnesh Modh <contact@thespecialcharacter.com>
2024-02-21 12:02:40 +0530

    class component

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,52 @@
+/* eslint-disable react/state-in-constructor */
 import './style.css';
-import React from 'react';
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './components/header';
-import Banner from './components/banner';
-import Footer from './components/footer';
 
-// Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
+
+export default class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    const { count: data } = this.state;
+
+    return (
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState(({ count }) => ({
+              count: count + 1,
+            }));
+          }}
+        >
+          +
+        </button>
+        <p>{data}</p>
+        <button
+          type="button"
+          disabled={data <= 0}
+          onClick={() => {
+            this.setState(({ count }) => ({
+              count: count - 1,
+            }));
+          }}
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+}
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
 root.render(
   <div>
-    <Header />
-    <Banner />
-    <Footer />
+    <App msg="Helllo From props" />
+    {/* const app = new App({ msg: "hello from props"}) */}
   </div>,
 );
